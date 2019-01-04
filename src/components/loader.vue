c867115308c44190af55d30f6efdbd022943bc8c
YuLy <3056174113@qq.com>
2019-01-04 21:58:53 +0800

    add loader

---
+++ src/components/loader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="popwraper">
+        <div class='popcontent'>
+            加载中。。。
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:'loader',
+    data:function(){
+        return{}
+    },
+    methods:{
+        hideme:function(){
+            this.$emit('hidden');
+        }
+    }
+}
+</script>
+<style scoped>
+.popwraper{
+  position: fixed;
+  top:0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  z-index: 9999;
+  background-color: rgba(0,0,0,0.6);
+}
+.popcontent{
+  position: absolute;
+  top:50%;
+  right: 50%;
+  width: 200px;
+  height: 100px;
+  background-color: white;
+  margin-right: -100px;
+  margin-top: -50px;
+}
+</style>
